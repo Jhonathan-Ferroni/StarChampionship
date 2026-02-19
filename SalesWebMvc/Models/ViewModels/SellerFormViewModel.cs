8271143fe21fa152a91ac416c20d0cdf71eb0a84
Jhonathan-Ferroni <ferroni.jhonathan2403@gmail.com>
2026-02-19 13:33:33 -0300

    Initial commit: Structure from SalesWebCrud

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+
+        [ValidateNever]
+        public ICollection<Department> Departments { get; set; }
+        
+    }
+}
---
+++ SalesWebMvc/Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
+using System.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; 
+using Pomelo.EntityFrameworkCore.MySql;
+using SalesWebMvc.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
+var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+        options.UseMySql(
+            builder.Configuration.GetConnectionString("SalesWebMvcContext"),
+            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMvcContext")),
+            mySqlOptions => mySqlOptions.MigrationsAssembly("SalesWebMvc")
+        ));
+
+
+//Addscoped
+
+builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var seedingService = services.GetRequiredService<SeedingService>();
+        seedingService.Seed();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Erro ao executar o seeding");
+    }
+}
+
+//locale
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
+
+app.UseRequestLocalization(localizationOptions);
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
